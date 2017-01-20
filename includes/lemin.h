a53e1443b17860007d21aaba0b2174db42b424a2
Mickael DOS SANTOS <mdos-san@e2r6p12.42.fr>
2017-01-20 14:56:05 +0100

    rm useless file

--- includes/lemin.h
+++ includes/lemin.h
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/22 09:05:32 by mdos-san          #+#    #+#             */
-/*   Updated: 2017/01/20 14:53:37 by mdos-san         ###   ########.fr       */
+/*   Updated: 2017/01/20 14:55:16 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,8 +74,6 @@ typedef struct	s_lm
 t_lm			lm_get();
 void			lm_start(t_lm *lm);
 char			*ft_strdup_to_char(char *str, char c);
-void			ft_lstpushb_cpy(t_list *lst, void *content,
-					size_t content_size);
 int				ft_lstcount(t_list *l);
 int				path_init(t_lm *lm);
 void			resolve(t_lm *lm);
